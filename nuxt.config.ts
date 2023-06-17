ece4379c251ae0c621e27f10080b9cdcf3b0dc58
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2023-06-17 10:34:09 +0800

    Pwa installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
+import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
+    modules: ["@vite-pwa/nuxt"],
+    devtools: { enabled: false },
     css: [
         "vuetify/lib/styles/main.sass",
         "@fortawesome/fontawesome-free/css/all.min.css",
