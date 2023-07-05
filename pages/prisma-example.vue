559cb7e4b4046a7bde631c32e8d0842ede596398
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-07-05 17:54:50 +0800

    configure Prisma

---
+++ pages/prisma-example.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data: examples } = useFetch("/api/examples");
+</script>
+
+<template>
+  <div>
+    Prisma ORM Data from the database, received
+    {{ examples?.length || 0 }} records:
+    <pre>{{ examples }}</pre>
+  </div>
+</template>
