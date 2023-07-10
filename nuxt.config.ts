41f4f135ada10f66134126a49bbabe3b11abacf0
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-07-10 15:34:57 +0800

    setup auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@vite-pwa/nuxt"],
-    devtools: { enabled: false },
-    css: [
-        "vuetify/lib/styles/main.sass",
-        "@fortawesome/fontawesome-free/css/all.min.css",
-    ],
-    build: {
-        transpile: ["vuetify"],
-    },
+  modules: ["@sidebase/nuxt-auth", "@vite-pwa/nuxt"],
+  devtools: { enabled: false },
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@fortawesome/fontawesome-free/css/all.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  typescript: {
+    shim: false,
+  },
 });
---
+++ pages/login-example.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <v-btn @click="signIn('google')">Login with Google</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: "/",
+  },
+});
+
+const { signIn } = useSession();
+</script>
