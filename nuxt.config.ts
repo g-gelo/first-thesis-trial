2ebcb1ae3c7ec1ab228d319014a54125b803cb87
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-07-10 16:05:13 +0800

    config auth origin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   modules: ["@sidebase/nuxt-auth", "@vite-pwa/nuxt"],
   devtools: { enabled: false },
+  auth: {
+    origin: process.env.AUTH_ORIGIN,
+  },
   css: [
     "vuetify/lib/styles/main.sass",
     "@fortawesome/fontawesome-free/css/all.min.css",
