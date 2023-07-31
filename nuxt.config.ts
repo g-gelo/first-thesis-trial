c52a0517b78d659a8ea491ebcb98fbeadb3e99de
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2023-07-31 10:10:20 +0800

    Revert "Fist Trial Messenger"
    This reverts commit d5b0d1918e5ee51cced1fb0ff2243de1f54b2c19.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,19 @@
 import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@sidebase/nuxt-auth", "@vite-pwa/nuxt"],
-    devtools: { enabled: false },
-    auth: {
-        origin: process.env.AUTH_ORIGIN,
-    },
-    css: [
-        "vuetify/lib/styles/main.sass",
-        "@fortawesome/fontawesome-free/css/all.min.css",
-    ],
-    build: {
-        transpile: ["vuetify"],
-    },
-    typescript: {
-        shim: false,
-    },
-    plugins: [{ src: "~/plugins/vue-fb-customer-chat.js", ssr: false }],
+  modules: ["@sidebase/nuxt-auth", "@vite-pwa/nuxt"],
+  devtools: { enabled: false },
+  auth: {
+    origin: process.env.AUTH_ORIGIN,
+  },
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@fortawesome/fontawesome-free/css/all.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  typescript: {
+    shim: false,
+  },
 });
