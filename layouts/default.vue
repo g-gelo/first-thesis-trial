fc6856c0baef9c43300319977dda96f98a569167
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2023-08-07 10:25:56 +0800

    Revert "Facebook Messenger Plugin"
    This reverts commit b3108d0e00e28addce0a00498f7b876e6113f919.

--- layouts/default.vue
+++ layouts/default.vue
@@ -28,9 +28,6 @@
                 </div>
             </template>
         </v-main>
-        <div>
-            <FacebookChat />
-        </div>
         <v-footer class="sticky-bottom" color="bg200">
             <v-row justify="center" align="center" no-gutters>
                 <v-btn
@@ -68,7 +65,6 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import FacebookChat from "~/components/FacebookChat.vue";
 
 const navItem = ref([
     { icon: "fa-solid fa-house", text: "Home", route: "/" },
