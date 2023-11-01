7883fb6ba645f5c43f5df09ecf75724aeb9e10b5
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2023-11-01 01:42:56 -0400

    New Card/Seminar/Career

--- components/TheEmergency.vue
+++ components/TheEmergency.vue
@@ -1,62 +1,47 @@
 <template>
-    <v-card
+    <div
         v-for="emergency in hotline"
         :key="emergency.id"
-        class="mb-6 rounded-xl"
+        class="mb-6 rounded-xl bg-white"
     >
-        <v-row class="content-center-left">
-            <v-col
-                cols="4"
-                class="border-left rounded-tl-xl rounded-bl-xl space-on-left"
-            >
-                <div>
-                    <v-img src="/img/careersblob.svg" class="top-image"
-                        ><v-img
-                            class="seminar-icon"
-                            src="/img/emergencyIcon.png"
-                            width="75px"
-                            height="75px"
-                        ></v-img
-                    ></v-img>
+        <div class="flex items-start">
+            <div class="w-24 h-24 bg-secondary-100 relative">
+                <div class="ma-2">
+                    <img
+                        src="/img/careersblob.svg"
+                        alt="Emergency Blob"
+                        class="top-image absolute top-0 left-0"
+                    />
+                    <img
+                        src="/img/emergencyIcon.png"
+                        alt="Emergency Icon"
+                        class="absolute"
+                    />
                 </div>
-            </v-col>
-            <v-col cols="7">
-                <v-list class="seminar-details">
-                    <v-list-item-group>
-                        <v-list-item>
-                            <v-list-item-content>
-                                <div class="mt-1 seminar-info">
-                                    <span
-                                        class="info-value main-title highlight"
-                                        >{{ emergency.organization }}</span
-                                    >
-                                </div>
-                                <div class="mt-5 ma-2 seminar-info">
-                                    <!-- Styling for the Where icon -->
-                                    <v-icon start class="icon-small"
-                                        >fa-solid fa-location-dot</v-icon
-                                    >
-                                    <span class="info-value info-small pa-0">{{
-                                        emergency.location
-                                    }}</span>
-                                    <div class="mt-5 ma-2 seminar-info">
-                                        <!-- Styling for the Where icon -->
-                                        <v-icon start class="icon-small"
-                                            >fa-solid fa-phone</v-icon
-                                        >
-                                        <span
-                                            class="info-value info-small pa-0"
-                                            >{{ emergency.number }}</span
-                                        >
-                                    </div>
-                                </div>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
-            </v-col>
-        </v-row>
-    </v-card>
+            </div>
+            <div class="w-60 p-4 border-l-4 border-green-800">
+                <div class="mt-1 seminar-info">
+                    <span class="info-value main-title highlight">{{
+                        emergency.organization
+                    }}</span>
+                </div>
+                <div class="mt-5 ma-2 seminar-info">
+                    <v-icon class="icon-small mr-3"
+                        >fa-solid fa-location-dot</v-icon
+                    >
+                    <span class="info-value info-small pa-0">{{
+                        emergency.location
+                    }}</span>
+                </div>
+                <div class="mt-5 ma-2 seminar-info">
+                    <v-icon class="icon-small mr-3">fa-solid fa-phone</v-icon>
+                    <span class="info-value info-small pa-0">{{
+                        emergency.number
+                    }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -64,12 +49,6 @@ const { data: hotline } = useFetch("/api/hotline");
 </script>
 
 <style scoped>
-.content-center-left {
-    justify-content: flex-start;
-}
-.border-left {
-    border-right: 1px solid green;
-}
 .highlight {
     font-weight: bold;
 }
@@ -78,10 +57,7 @@ const { data: hotline } = useFetch("/api/hotline");
     font-weight: bold;
     margin-bottom: 10px;
 }
-.space-on-left {
-    padding-left: 10px;
-    margin-right: 10px;
-}
+
 .info-small {
     font-size: 14px;
 }
