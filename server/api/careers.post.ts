353ec4c4cec1aa75c873d031c0a76196532148ab
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2023-11-30 13:45:54 +0800

    attempt to fix email sending

--- server/api/careers.post.ts
+++ server/api/careers.post.ts
@@ -1,31 +1,28 @@
 import sendEmail from "../../utils/sendEmail";
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
-    let career = null;
+  const body = await readBody(event);
+  let career = null;
 
-    if (
-        body.title &&
-        body.description &&
-        body.date &&
-        body.time &&
-        body.location
-    )
-        await event.context.prisma.career
-            .create({
-                data: {
-                    title: body.title,
-                    description: body.description,
-                    date: body.date,
-                    time: body.time,
-                    location: body.location,
-                },
-            })
-            .then(async (response) => {
-                career = response;
-                await sendEmail("Careers @ CvSU", "Hiring! 1 English");
-            });
-    return {
-        career,
-    };
+  if (
+    body.title &&
+    body.description &&
+    body.date &&
+    body.time &&
+    body.location
+  ) {
+    career = await event.context.prisma.career.create({
+      data: {
+        title: body.title,
+        description: body.description,
+        date: body.date,
+        time: body.time,
+        location: body.location,
+      },
+    });
+    await sendEmail("Careers @ CvSU", "Hiring! 1 English");
+  }
+  return {
+    career,
+  };
 });
