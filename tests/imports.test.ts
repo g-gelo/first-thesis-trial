e0f71db5d1ee604ab85c030035ab322586cfa2b9
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-04-17 19:19:45 +0800

    Last na potangina yehey

--- tests/imports.test.ts
+++ tests/imports.test.ts
@@ -1,20 +1,20 @@
 import { test, expect } from "vitest";
 
 describe("import vue components", () => {
-    test("normal imports as expected", async () => {
-        const cmp = await import("../components/CareerForm.vue");
-        expect(cmp).toBeDefined();
-    });
+  test("normal imports as expected", async () => {
+    const cmp = await import("../components/CareerForm.vue");
+    expect(cmp).toBeDefined();
+  });
 });
 describe("import vue components", () => {
-    test("normal imports as expected", async () => {
-        const cmp = await import("../components/EmergencyHotline.vue");
-        expect(cmp).toBeDefined();
-    });
+  test("normal imports as expected", async () => {
+    const cmp = await import("../components/EmergencyHotline.vue");
+    expect(cmp).toBeDefined();
+  });
 });
 describe("import vue components", () => {
-    test("normal imports as expected", async () => {
-        const cmp = await import("../components/TawkChat.vue");
-        expect(cmp).toBeDefined();
-    });
+  test("normal imports as expected", async () => {
+    const cmp = await import("../components/TheEmergency.vue");
+    expect(cmp).toBeDefined();
+  });
 });
