5d19c8bfef14855a4d993432434ca0a68783c3d0
Angelo Gabriel D. Evangelista <104245100+g-gelo@users.noreply.github.com>
2024-06-25 17:42:30 +0800

    Migrating to Neon

--- components/TheTitle.vue
+++ components/TheTitle.vue
@@ -1,50 +1,48 @@
 <template>
-    <v-container
-        gutter
-        class="custom-container"
-        :style="{
-            backgroundColor:
-                $vuetify.theme.themes.myCustomLightTheme.colors.bg100,
-            backgroundImage: `url(${backgroundImage}), url(${svgBlob})`,
-        }"
+  <v-container
+    gutter
+    class="custom-container"
+    :style="{
+      backgroundColor: $vuetify.theme.themes.myCustomLightTheme.colors.bg100,
+      backgroundImage: `url(${backgroundImage}), url(${svgBlob})`,
+    }"
+  >
+    <h1
+      class="custom-title custom-center"
+      :style="{
+        color: $vuetify.theme.themes.myCustomLightTheme.colors.primary300,
+      }"
     >
-        <h1
-            class="custom-title custom-center"
-            :style="{
-                color: $vuetify.theme.themes.myCustomLightTheme.colors
-                    .primary300,
-            }"
-        >
-            {{ title }}
-        </h1>
-    </v-container>
+      {{ title }}
+    </h1>
+  </v-container>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-    title: string;
-    backgroundImage: string;
-    svgBlob: string;
+  title: string;
+  backgroundImage: string;
+  svgBlob: string;
 }>();
 </script>
 
 <style scoped>
 .custom-container {
-    height: 20vh;
-    width: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-start;
-    background-size: 50%;
-    background-repeat: no-repeat;
-    background-position: right bottom;
+  height: 20vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  background-size: 50%;
+  background-repeat: no-repeat;
+  background-position: right bottom;
 }
 .custom-title {
-    font-size: 24px;
-    font-weight: bold;
-    margin: 10% 10% 12% 4%;
-    border-left: 5px solid #286400;
-    padding-left: 1rem;
+  font-size: 24px;
+  font-weight: bold;
+  margin: 10% 10% 12% 4%;
+  border-left: 5px solid #286400;
+  padding-left: 1rem;
 }
 </style>
